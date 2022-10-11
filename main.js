1f46627413c27a584b503bde08ddbb0c685702cf
Владимир Ермаков <de1mos92@mail.ru>
2022-10-11 23:22:03 +0300

    izmeneniya

---
+++ main.js
@@ -0,0 +1,32 @@
+// let asd
+// asd = 'vova 4ert'
+// let a = 1
+// let b = 34
+// let slitno = 3 > 4
+// // console.log(`${asd}${a + b}`,asd)
+// console.log(asd, a, slitno)
+
+// // let value = 5>3;
+// // console.log(value); // string
+// // value = String(value); // теперь value это строка "true"
+// // console.log(value); // string
+
+
+// for (let index = 0; index < 10; ) {
+// 	let a = index++;
+// 	console.log(a, index)
+
+
+// }
+
+// for (a = 5, b = 3; a < 10; a++) {
+	
+// 	c = a * b
+// 	console.log(a,b,c)
+//   }
+
+//   for (a = 1; a < 10; a++) {
+// 	console.log(a)
+//   }
+
+console.log(true + '1')
